aaa245a3f9b1ec7898789f57d3ca8c1f7ae4add0
valfur03 <valentin.furmanek@gmail.com>
2025-06-22 13:18:15 +0200

    refactor: move uploadFile into service

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,26 +1,15 @@
 import { Controller, HttpCode, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { writeFile } from 'node:fs/promises';
-import { ConfigService } from '@nestjs/config';
-import { ENV_WATCH_DIRECTORY_PATH_KEY } from '../common/constants/env';
+import { JobsService } from './jobs.service';
 
 @Controller('jobs')
 export class JobsController {
-  private readonly watchDirectoryPath: string;
-  private readonly inputWatchDirectoryPath: string;
-
-  constructor(private readonly configService: ConfigService) {
-    this.watchDirectoryPath = configService.getOrThrow(ENV_WATCH_DIRECTORY_PATH_KEY);
-    this.inputWatchDirectoryPath = `${this.watchDirectoryPath}/in`;
-  }
+  constructor(private readonly jobsService: JobsService) {}
 
   @Post()
   @HttpCode(HttpStatus.ACCEPTED)
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    const temporaryUniqueFileId = crypto.randomUUID();
-    const inputFilePath = `${this.inputWatchDirectoryPath}/${temporaryUniqueFileId}-${file.originalname}`;
-
-    await writeFile(inputFilePath, file.buffer);
+    await this.jobsService.uploadFile(file);
   }
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { writeFile } from 'node:fs/promises';
+import { ENV_WATCH_DIRECTORY_PATH_KEY } from '../common/constants/env';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class JobsService {}
+export class JobsService {
+  private readonly watchDirectoryPath: string;
+  private readonly inputWatchDirectoryPath: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.watchDirectoryPath = this.configService.getOrThrow(ENV_WATCH_DIRECTORY_PATH_KEY);
+    this.inputWatchDirectoryPath = `${this.watchDirectoryPath}/in`;
+  }
+
+  async uploadFile(file: Express.Multer.File) {
+    const temporaryUniqueFileId = crypto.randomUUID();
+    const inputFilePath = `${this.inputWatchDirectoryPath}/${temporaryUniqueFileId}-${file.originalname}`;
+
+    await writeFile(inputFilePath, file.buffer);
+  }
+}
