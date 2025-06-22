3f5c314afc61f8d37213c06193949168c2ff0598
valfur03 <valentin.furmanek@gmail.com>
2025-06-22 11:07:58 +0200

    feat: save uploaded file into in/ directory

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,22 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-import { existsSync, mkdirSync } from 'node:fs';
+import { mkdirSync } from 'node:fs';
 import { ConfigService } from '@nestjs/config';
 import { ENV_WATCH_DIRECTORY_PATH_KEY } from './common/constants/env';
 
 @Injectable()
 export class AppService implements OnApplicationBootstrap {
   private readonly watchDirectoryPath: string;
+  private readonly inputWatchDirectoryPath: string;
+  private readonly outputWatchDirectoryPath: string;
 
   constructor(private readonly configService: ConfigService) {
-    this.watchDirectoryPath = configService.getOrThrow(ENV_WATCH_DIRECTORY_PATH_KEY);
+    this.watchDirectoryPath = this.configService.getOrThrow(ENV_WATCH_DIRECTORY_PATH_KEY);
+    this.inputWatchDirectoryPath = `${this.watchDirectoryPath}/in`;
+    this.outputWatchDirectoryPath = `${this.watchDirectoryPath}/out`;
   }
 
   onApplicationBootstrap() {
-    if (!existsSync(this.watchDirectoryPath)) {
-      mkdirSync(this.watchDirectoryPath);
-    }
+    mkdirSync(this.inputWatchDirectoryPath, { recursive: true });
+    mkdirSync(this.outputWatchDirectoryPath, { recursive: true });
   }
 }
--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -7,9 +7,11 @@ import { ENV_WATCH_DIRECTORY_PATH_KEY } from '../common/constants/env';
 @Controller('jobs')
 export class JobsController {
   private readonly watchDirectoryPath: string;
+  private readonly inputWatchDirectoryPath: string;
 
   constructor(private readonly configService: ConfigService) {
     this.watchDirectoryPath = configService.getOrThrow(ENV_WATCH_DIRECTORY_PATH_KEY);
+    this.inputWatchDirectoryPath = `${this.watchDirectoryPath}/in`;
   }
 
   @Post()
@@ -17,7 +19,7 @@ export class JobsController {
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     const temporaryUniqueFileId = crypto.randomUUID();
-    const inputFilePath = `${this.watchDirectoryPath}/${temporaryUniqueFileId}-${file.originalname}`;
+    const inputFilePath = `${this.inputWatchDirectoryPath}/${temporaryUniqueFileId}-${file.originalname}`;
 
     await writeFile(inputFilePath, file.buffer);
   }
